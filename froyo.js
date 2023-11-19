6e656c7173ffdfe56d110f8ab324a4110b822940
Jaron Proehl <jaron@strategicmissionelements.com>
2023-11-19 15:58:49 -0500

    Third edit

--- froyo.js
+++ froyo.js
@@ -1,15 +1,15 @@
 // I am adding a prompt, which will ask the user to input Froyo Flavors
+let userObject = {};
 let froyoFlavors = prompt("Please enter a comma-seperated list of froyo flavors!");
+userObject.data = froyoFlavors;
 
 //The following code will split the user's comma-seperated input into an array of strings.
 const input = froyoFlavors.split(', ');
 
-// The following code will be used to iterate through the array of flavors
-let totalFlavors = 0;
-for (const individualFlavor in input) {
-  totalFlavors += input[individualFlavor];
+//The following code will be used to iterate through the array of strings
+for (var i = 0; i < input.length; i++) {
+  console.log(input[i]);
 }
-console.log(totalFlavors);
 
 // The following code will count the number of of each flavor in the user's input string.
 console.log(Object.values(input));
