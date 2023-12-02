ab1e32fe0d17262113c3a42438576f02117e9345
Jaron Proehl <jaron@strategicmissionelements.com>
2023-12-02 15:34:00 -0500

    2nd submission.

--- froyo.js
+++ froyo.js
@@ -6,10 +6,19 @@ userObject.data = froyoFlavors;
 //The following code will split the user's comma-seperated input into an array of strings.
 const input = froyoFlavors.split(', ');
 
+let flavorCounts = {};
+
 //The following code will be used to iterate through the array of strings
 for (var i = 0; i < input.length; i++) {
-  console.log(input[i]);
+  let flavor = input[i].trim();
+  if (!flavorCounts[flavor]) {
+    flavorCounts[flavor] = 1;
+  } else {
+    flavorCounts[flavor]++;
+  }
 }
+console.log("Flavor counts:");
+console.log(flavorCounts);
 
 // The following code will count the number of of each flavor in the user's input string.
 console.log(Object.values(input));
